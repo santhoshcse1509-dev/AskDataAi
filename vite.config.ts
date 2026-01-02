283a741102071ddcc07c0aa1f25b5c832312c996
sanja <sanja@example.com>
2026-01-02 20:34:31 +0530

    Configure project for automated deployment to Vercel and GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // Makes deployment to sub-paths (like GitHub Pages) and Vercel work seamlessly
       server: {
         port: 3000,
         host: '0.0.0.0',
